a816c2c26e664e3491874e57252a923698c081cb
S-Lombardi <silo.lombardi@gmail.com>
2023-05-23 19:12:15 +0200

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,2 +1,65 @@
 "use strics";
-console.log(1)
+
+//Inserisco le immagini in un array
+let pics = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+]
+
+//variabile per inserire tag e immagini 
+let boxImg = "";
+
+//Creo i tag e inserisco le immagini
+for(let i=0 ; i < pics.length; i++ ) {
+    
+    boxImg +=   `<div class="box-img">
+                    <img src="${pics[i]}">
+                </div>`
+   
+}
+
+//Inserisco la struttura creata in precedenza nella pagina 
+document.getElementById('wrap-slide').innerHTML = boxImg;
+
+//Rendo display:block la prima immagine inserendo la classe block
+document.querySelector(".box-img").classList.add("block");
+
+//variabili per recuperare i due pulsanti
+let buttonUp = document.getElementById("up");
+let buttonDown = document.getElementById("down");
+
+//varabile per recuperare le immagini
+let allBoxImg = document.getElementsByClassName("box-img");
+
+//variabile per indice immagine
+let indiceImmagine = 0;
+
+
+//pulsante Up
+buttonUp.addEventListener("click", function(){
+
+    if(indiceImmagine < allBoxImg.length - 1) {
+        allBoxImg[indiceImmagine].classList.remove("block");
+
+        indiceImmagine = indiceImmagine + 1;
+
+        allBoxImg[indiceImmagine].classList.add("block");
+    }
+
+});
+
+//pulsante Down
+buttonDown.addEventListener("click", function(){
+
+    if(indiceImmagine > 0) {
+        allBoxImg[indiceImmagine].classList.remove("block");
+
+        indiceImmagine = indiceImmagine - 1;
+
+        allBoxImg[indiceImmagine].classList.add("block");
+    }
+
+});
