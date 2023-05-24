6aeeca9aa387239b2e775c92bc8c7d00a71a05ed
S-Lombardi <silo.lombardi@gmail.com>
2023-05-24 23:49:19 +0200

    Aggiunte thumbnails

--- bonus2/js/script.js
+++ bonus2/js/script.js
@@ -11,7 +11,7 @@ let pics = [
 
 //variabile per inserire tag e immagini 
 let boxImg = "";
-
+let boxImgTmb = "";
 //Creo i tag e inserisco le immagini
 for(let i=0 ; i < pics.length; i++ ) {
     
@@ -19,10 +19,17 @@ for(let i=0 ; i < pics.length; i++ ) {
                     <img src="${pics[i]}">
                 </div>`
    
+    boxImgTmb += `<div class="thumbnail ">
+                    <img class="tmb_${i}" src="${pics[i]}">
+                 </div>`
+
 }
+
+
 
 //Inserisco la struttura creata in precedenza nella pagina 
 document.getElementById('wrap-slide').innerHTML = boxImg;
+document.getElementById('wrap-thumbnail').innerHTML += boxImgTmb;
 
 //Rendo display:block la prima immagine inserendo la classe block
 document.querySelector(".box-img").classList.add("block");
@@ -38,18 +45,33 @@ let allBoxImg = document.getElementsByClassName("box-img");
 let indiceImmagine = 0;
 
 
+//FILTRO THUMBNAIL 
+document.querySelector(".thumbnail").classList.add("no-filter");
+
+let allBoxTmb = document.getElementsByClassName("thumbnail");
+
+
 //pulsante Up
 buttonUp.addEventListener("click", function(){
     
     if(indiceImmagine > 0) {
         allBoxImg[indiceImmagine].classList.remove("block");
-        indiceImmagine = indiceImmagine - 1;
+        allBoxTmb[indiceImmagine].classList.remove("no-filter");
+
+        indiceImmagine = indiceImmagine - 1; //indiceImmagine--
+    
         allBoxImg[indiceImmagine].classList.add("block");
+        allBoxTmb[indiceImmagine].classList.add("no-filter");
+
     }
     else{
         allBoxImg[indiceImmagine].classList.remove("block");
+        allBoxTmb[indiceImmagine].classList.remove("no-filter");
+
         indiceImmagine = allBoxImg.length -1;
+
         allBoxImg[indiceImmagine].classList.add("block");
+        allBoxTmb[indiceImmagine].classList.add("no-filter");
     }
 });
 
@@ -58,15 +80,21 @@ buttonDown.addEventListener("click", function(){
 
     if(indiceImmagine < allBoxImg.length -1 ){
         allBoxImg[indiceImmagine].classList.remove("block");
+        allBoxTmb[indiceImmagine].classList.remove("no-filter");
 
         indiceImmagine = indiceImmagine +1 ;
 
         allBoxImg[indiceImmagine].classList.add("block");
+        allBoxTmb[indiceImmagine].classList.add("no-filter");
     }
     else{
         allBoxImg[indiceImmagine].classList.remove("block");
+        allBoxTmb[indiceImmagine].classList.remove("no-filter");
+
         indiceImmagine = 0;
+
         allBoxImg[indiceImmagine].classList.add("block");
+        allBoxTmb[indiceImmagine].classList.add("no-filter");
     }
 
 });
